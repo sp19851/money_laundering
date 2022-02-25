8ad50f3ceb5c881a545a1abfbcb31417c70cd26d
unknown <sp19851@gmail.com>
2022-02-25 16:55:24 +0200

    prepare

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Money laundering for QBCore by @Cruso#5044'
+version '2.0.0'
+
+shared_scripts {
+    'config.lua',
+	
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
+
